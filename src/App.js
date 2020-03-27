5f24f0d8567fccc4682bdfe3d914a273d44e4ef4
Henry Nelson <Henry.Nelson89@gmail.com>
2020-03-27 08:54:17 -0700

    I had to restart to fix a bug getting moving now

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from "react";
-
+import Home from "./components/home"
+import { Route } from "react-router-dom";
 const App = () => {
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <Route exact path="/" componant={Home} />
     </>
   );
 };
