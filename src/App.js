b6d9b4205bdce9fca667b1eaadd1b5d83fcf3a7d
Lisa Maskovich <lisamariejensenmaskovich@gmail.com>
2020-03-27 16:37:44 -0700

    Working on testing

--- src/App.js
+++ src/App.js
@@ -1,17 +1,37 @@
 import React from "react";
-import {Route, Link} from "react-router-dom";
-import pizza_form from "./component/pizza_form";
+import Pizza_Form from "./component/pizza_form";
+import Home from "./component/Home";
+import {BrowserRouter as Router, Route, Link} from "react-router-dom"
 
-const App = () => {
-  const []
+function App() {
+
   return (
-    <>
+<div>
+
+      <nav className="navbar">
+          <Link to="/"> Home
+            {/* <button name = 'homebutton'>Home</button> */}
+            </Link>
+          <br/>
+          <Link to="/component/pizza_form"> Order a Pizza!
+          {/* <button to name = 'orderbutton'>Order a Pizza!</button> */}
+          </Link>
+
+      </nav>
       <h1>Lambda Eats</h1>
       <p>Hot pizza coming your way!</p>
-      <div>
-        <Route exact path="./component/pizza_form" component={pizza_form} />
+      <div className="App">
+
+        <Route exact path="/">
+          <Home/>
+        </Route>
+
+        <Route path="/component/pizza_form">
+          <Pizza_Form/>
+        </Route>
+
       </div>
-    </>
+    </div>
   );
 };
 export default App;
---
+++ src/component/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+
+function Home() {
+  return (
+    <div className="home">
+      <h1>Pizza for You!</h1>
+    </div>
+  );
+}
+
+export default Home;
