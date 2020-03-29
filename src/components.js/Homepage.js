62c3e4b273ab05cd01c8f96c39ed6ccb17089159
DenisChuvilin <denischuv@gmail.com>
2020-03-28 17:25:36 -0700

    both errors work

--- src/components.js/Homepage.js
+++ src/components.js/Homepage.js
@@ -1,25 +1,19 @@
-import React from "react"
-import { Link } from "react-router-dom"
-
+import React from 'react';
+import { Link } from 'react-router-dom';
+import homePizzaImg from './Pizza.jpg';
+import './Homepage.css';
 const Homepage = () => {
+  return (
+    <div class="home_wrapper">
+      <h1 className="title">Lambda Eats</h1>
+      <img className="homePizzaImg" src={homePizzaImg} alt="homepage_image" />
+      <Link class="link" to="/pizza">
+        
+          <button class="orderButton">Order a Pizza!</button>
+      
+      </Link>
+    </div>
+  );
+};
 
-    return (
-<div className = "home-wrapper">
-    <img 
-    className = "home-image"
-    src="./Pizza.jpg"
-    alt = "homepage_image"
-    />
-<Link to ="/pizza">
-      <button>
-        Order a Pizza!
-    </button>
-</Link>
-  
-
-
-</div>
-    )
-}
-
-export default Homepage
+export default Homepage;
--- src/components.js/Pizza.js
+++ src/components.js/Pizza.js
@@ -3,18 +3,16 @@ import { Link } from 'react-router-dom';
 import * as yup from 'yup';
 import axios from 'axios';
 
-import "./Pizza.css"
+import './Pizza.css';
 
 const formSchema = yup.object().shape({
-  name: yup
-    .string()
-    .min(2, "come on, your name is longer than that!"),
-  pizza_size: yup.string(),
-  veggies: yup.boolean().oneOf([true], 'I know you want the veggies'),
-  Pineapple: yup.boolean().oneOf([true], 'I know you want the Pineappple'),
-  BBQ: yup.boolean().oneOf([true], 'I know you want the BBQ'),
-  Bacon: yup.boolean().oneOf([true], 'I know you want the Bacon'),
-  special_instructions: yup.string().required('I am a robot, i need to know where to leave the pizza'),
+  name: yup.string().min(2, 'come on, your name is longer than that!'),
+  pizza_size: yup.string().oneOf(['XS', 'S', 'M','L',"XL"], "Do you want a pizza, or not?")
+  // veggies: yup.boolean().oneOf([true], 'I know you want the veggies'),
+  // Pineapple: yup.boolean().oneOf([true], 'I know you want the Pineappple'),
+  // BBQ: yup.boolean().oneOf([true], 'I know you want the BBQ'),
+  // Bacon: yup.boolean().oneOf([true], 'I know you want the Bacon'),
+  // special_instructions: yup.string().required('I am a robot, i need to know where to leave the pizza'),
 });
 
 const Pizza = () => {
@@ -26,17 +24,17 @@ const Pizza = () => {
     Bacon: false,
     Pineapple: false,
     BBQ: false,
-    special_instructions: '',
+    special_instructions: ''
   });
   // state for errors
   const [errors, setErrors] = useState({
     name: '',
     pizza_size: '',
-    veggies: '',
-    Bacon: '',
-    Pineapple: '',
-    BBQ: '',
-    special_instructions: '',
+    // veggies: '',
+    // Bacon: '',
+    // Pineapple: '',
+    // BBQ: '',
+    // special_instructions: '',
   });
   // state for submit button
   const [disableButton, setDisableButton] = useState(true);
@@ -51,7 +49,7 @@ const Pizza = () => {
   //validaton and change
   function validateChange(e) {
     yup
-      .reach(formSchema, e.target.name)
+      .reach(formSchema,  (e.target.type === "checkbox" || e.target.type === "textarea") ? null : e.target.name)
       .validate(e.target.value)
       .then(valid => {
         setErrors({ ...errors, [e.target.name]: '' });
@@ -67,9 +65,8 @@ const Pizza = () => {
       ...formValues,
       [e.target.name]: e.target.type === 'checkbox' ? e.target.checked : e.target.value,
     };
-    setFormValues(updatedData);
     validateChange(e);
-
+    setFormValues(updatedData);
   }
   // post data  on Submit
   function submitForm(e) {
@@ -97,26 +94,24 @@ const Pizza = () => {
     <div>
       <Link to="/">Home</Link>
 
-      <form  onSubmit={submitForm}>
+      <form onSubmit={submitForm}>
         <label htmlFor="Name">
           Customer Name:
-          <input 
-          name="name"
-          value = {formValues.name}
-          onChange = {inputChange}
-           />
+          <input name="name" value={formValues.name} onChange={inputChange} />
           {errors.name.length > 0 ? <p className="error">{errors.name}</p> : null}
         </label>
         <br />
         <label htmlFor="Pizza Size">
           Pizza Size:
-          <select id="size" name="pizza_size">
+          <select name="pizza_size" onChange={inputChange}>
+          <option> Select a Size</option>
             <option value="XS">XS</option>
             <option value="S">S</option>
             <option value="M">M</option>
             <option value="L">L</option>
             <option value="XL">XL</option>
           </select>
+          {errors.pizza_size.length > 0 ? <p className="error">{errors.pizza_size}</p> : null}
         </label>
         <br />
         <label htmlFor="toppings" className="toppings">
@@ -137,7 +132,6 @@ const Pizza = () => {
             checked={formValues.Bacon}
             value={formValues.Bacon}
             onChange={inputChange}
-            
           />
           Bacon
         </label>{' '}
@@ -149,32 +143,19 @@ const Pizza = () => {
             checked={formValues.Pineapple}
             value={formValues.Pineapple}
             onChange={inputChange}
-        
           />
           Pineapple
         </label>
         <br />
         <label htmlFor="toppings" className="toppings">
-          <input
-            type="checkbox"
-            name="BBQ"
-            checked={formValues.BBQ}
-            value={formValues.BBQ}
-            onChange={inputChange}
-            
-          />
+          <input type="checkbox" name="BBQ" checked={formValues.BBQ} value={formValues.BBQ} onChange={inputChange} />
           BBQ
         </label>
         <label>
           <br />
           Special Instructions:
           <br />
-          <textarea
-            name="special_instructions"
-            value={formValues.special_instructions}
-            onChange={inputChange}
-        
-          />
+          <textarea type= "textarea" name="special_instructions" value={formValues.special_instructions} onChange={inputChange} />
         </label>
         <br />
         <pre>{JSON.stringify(post, null, 2)}</pre>
