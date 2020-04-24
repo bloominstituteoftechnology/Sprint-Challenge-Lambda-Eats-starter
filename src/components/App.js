2698cd6f7ddf612f557890b6da0f2caa33a655b5
Blayze <blayzestone@protonmail.com>
2020-04-24 09:12:16 -0700

    feat: setup routes for rendering

--- src/components/App.js
+++ src/components/App.js
@@ -4,19 +4,25 @@ import React from "react";
 import Header from './Header';
 import Hero from './Hero';
 import CTA from './CTA';
-import { BrowserRouter as Router, Link } from 'react-router-dom';
+import { Route, Switch, Link } from 'react-router-dom';
 
 const App = () => {
   return (
-    <Router>
+    <>
       <Header>
         <h1>Lambda Eats</h1>
         <Link className="h5" exact to="/">Home</Link>
       </Header>
-      <Hero>
-        <CTA />
-      </Hero>
-    </Router>
+
+      <Switch>
+        <Route path="/pizza"></Route>
+        <Route path="/">
+          <Hero>
+            <CTA />
+          </Hero>
+        </Route>
+      </Switch>
+    </>
   );
 };
 export default App;
