e0c1a5e85c82f9f76d41572e5fc88af85ccb3291
Blayze <blayzestone@protonmail.com>
2020-04-24 08:46:57 -0700

    feat: Header component
    The header component is fixed to the top of the screen. it contains the
    Logo (Lambda Eats) and will contain the home link.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 
 // Components
+import Header from './Header';
 import Hero from './Hero';
 
 const App = () => {
   return (
     <>
+      <Header>
+        <h1>Lambda Eats</h1>
+      </Header>
       <Hero>
       </Hero>
     </>
---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Header = styled.header`
+  width: 100%;
+  position: fixed;
+  display: flex;
+  justify-content: space-between;
+  background-color: #DCDCDC;
+  top: 0;
+  padding: 0.5rem 1rem;
+`;
+
+export default Header;
