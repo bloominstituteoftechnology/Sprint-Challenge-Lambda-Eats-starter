2c58aee274f0991c686436767e68f19fddea88b3
Blayze <blayzestone@protonmail.com>
2020-04-24 10:01:24 -0700

    feat: add form inputs to state

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Switch, Link } from 'react-router-dom';
 
 
@@ -10,17 +10,55 @@ import Form from './Form';
 import FormContainer from "./FormContainer";
 
 const App = () => {
+  const initialFormValues = {
+    size: '',
+    sauce: '',
+    pepperonni: '',
+    olives: '',
+    sausage: '',
+    extraCheese: '',
+    veggies: '',
+    instructions: '', 
+  }
+
+  const [formValues, setFormValues] = useState(initialFormValues);
+
+  const onInputChange = evt => {
+    const name = evt.target.name;
+    const value = evt.target.value;
+    const type = evt.target.type;
+
+    if(type === "checkbox") {
+      const isChecked = evt.target.checked;
+
+      return isChecked
+        ? setFormValues({
+          ...formValues,
+          [name]: value,
+        })
+        : setFormValues({
+          ...formValues,
+          [name]: '',
+        })
+    }
+
+    return setFormValues({
+      ...formValues,
+      [name]: value,
+    })
+  }
+
   return (
     <>
       <Header>
         <h1>Lambda Eats</h1>
-        <Link className="h5" exact to="/">Home</Link>
+        <Link className="h5" to="/">Home</Link>
       </Header>
 
       <Switch>
         <Route path="/pizza">
           <FormContainer>
-          <Form />
+          <Form changeHandler={onInputChange}/>
           </FormContainer>
         </Route>
         <Route path="/">
