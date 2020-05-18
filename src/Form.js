b893c135b503389f9c9092666adf8b97be8d9234
Luke Adams <muhammadadams@gmail.com>
2020-05-17 21:52:13 -0400

    inputs

--- src/Form.js
+++ src/Form.js
@@ -3,19 +3,36 @@ import React, { useState } from "react";
 //add state
 
 const [zaData, zaSetter] = useState({
-    name:
-    email: 
-    olive: 
-    jalapeno:
-    mushroom: 
-    feta:
-})
+  name: "",
+  email: "",
+  olive: "",
+  jalapeno: "",
+  mushroom: "",
+  feta: "",
+});
 
 export default function someZa(props) {
   return (
-    //inputs
+    //inputs for checkbox
     <div>
-      <form></form>
+      <form>
+        <label>
+          Olives
+          <input type="" name="" onChange="" />
+        </label>
+        <label>
+          Jalapeno
+          <input type="" name="" onChange="" />
+        </label>
+        <label>
+          Mushrooms
+          <input type="" name="" onChange="" />
+        </label>
+        <label>
+          Feta
+          <input type="" name="" onChange="" />
+        </label>
+      </form>
     </div>
   );
 }
