6291e382dbcb3a548a5252c7e6220791a389671e
larwat <lwtech20@gmail.com>
2020-06-13 11:06:11 -0700

    complete

---
+++ src/components/Home.js
@@ -0,0 +1,11 @@
+import React from 'react'; 
+
+function Home() {
+    return (
+        <div className="home">
+            <h1>Lambda Eats Pizza</h1>
+        </div>
+    );
+};
+
+export default Home
