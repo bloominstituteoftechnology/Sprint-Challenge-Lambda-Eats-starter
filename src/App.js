51706885acc8cf2da948a27598af1cfedf890ce1
Tylar Washington-Beechum <tylay47@gmail.com>
2020-06-13 00:02:27 -0400

    6/13/20

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from "react";
+import {Button, NavBar, Card, CardImg} from 'reactstrap'
+import {Link} from 'react-router-dom'
 
 const App = () => {
   return (
     <>
+    <NavBar>
       <h1>Lambda Eats</h1>
+      <Link to={'/'}>
+        <Button>
+          Home
+        </Button>
+      </Link>
+      </NavBar>
       <p>You can remove this code and create your own header</p>
     </>
   );
