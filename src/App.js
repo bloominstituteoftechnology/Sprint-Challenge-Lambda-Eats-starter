0436166c1e37ef9e2eb57c13022dfe36d6962a1e
Esteban Licea <esolo@Estebans-MacBook-Pro.local>
2020-06-15 20:05:11 -0700

    finished form components and started styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from "react";
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Form from './components/Form.js';
 
 const App = () => {
+
   return (
-    <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+    <div className="app">
+    <Router>
+      <div className = 'header'>
+          <h1 className = 'site-name'>Lambda Eats</h1>
+          
+          <button><Link to="/">Home</Link></button>
+          <Route exact path="/" />
+          <button><Link to="/pizza">Take Me To Pizza</Link></button>
+          <br></br>
+          <Route path="/pizza" component={Form} />
+      </div>
+    </Router>
+    </div>
   );
 };
+
 export default App;
